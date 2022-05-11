e6ba6d6e15e80a7fdc5c5e37a640822ce4a04f4b
Suraj Saroj <sarojsuraj@gmail.com>
2022-05-12 01:40:08 +0530

    first commit

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <>
+      <h1>Expense Item</h1>
+      <h2>Food Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
+    </>
+  );
+}
+
+export default ExpenseItem;
