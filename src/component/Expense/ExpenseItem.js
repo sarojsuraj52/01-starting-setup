40b6d729458b51ac5f005be3e97559c8c7dfea10
Suraj Saroj <sarojsuraj@gmail.com>
2022-05-13 12:03:25 +0530

    added useState

--- src/component/Expense/ExpenseItem.js
+++ src/component/Expense/ExpenseItem.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../Expense/ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
@@ -5,16 +6,22 @@ import Card from "./UI/Card";
 // import Expenses from './Expenses'
 
 function ExpenseItem(props) {
-const deletehandler = ()=>{
-  document.querySelector('.expense-item').remove()
-}
+  const [amount, setamount] = useState(props.amount);
+  const deletehandler = () => {
+    document.querySelector(".expense-item").remove();
+  };
+  const amounthandler = () => {
+    setamount('100$')
+    console.log(amount)
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <ExpenseDetails title={props.title} amount={props.amount}/>
+        <ExpenseDetails title={props.title} amount={amount} />
         <h3>{props.location}</h3>
         <button onClick={deletehandler}>Delete Expense</button>
+        <button onClick={amounthandler}>Change Price</button>
       </div>
     </Card>
   );
